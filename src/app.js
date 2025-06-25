68215262756747c71a859f7ed4177e6299638f4f
Raul Ucles <rucleslajara@gmail.com>
2025-06-25 21:20:40 +0000

    generador de excusas

--- src/app.js
+++ src/app.js
@@ -1,11 +1,19 @@
-import "bootstrap";
-import "./style.css";
-
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  
+    let who = ['El perro', 'Mi coche', 'Mi hermano', 'Un elefante'];
+
+    let action = ['se comió', 'se rompió', 'murió', 'saltó en paracaídas'];
+
+    let what = ['los deberes', 'cantando', 'volando','leyendo'];
+
+    let when = ['en la comunión', 'en la boda de mi madre', 'mientras entrenaba', 'cuando tenía 13 años'];
+
+    sust = who[Math.floor(Math.random() *who.length)] 
+    accion = action[Math.floor(Math.random() *action.length)]
+    que = what[Math.floor(Math.random() *what.length)]
+    cuando= when[Math.floor(Math.random() *when.length)]
+
+    frase = sust + ' ' + accion + ' ' + que + ' ' + cuando 
+    document.querySelector('#excusa').innerHTML = frase
 };
